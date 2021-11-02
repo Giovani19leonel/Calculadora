9e550b22310b461e55739e034415a57117203a40
Yoru <Yoru@YORU-PC>
2021-11-01 23:09:16 -0200

    Adicionar arquivos de projeto.

---
+++ Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        decimal valor1 = 0, valor2 = 0;
+        string operacao1 = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "0";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "1";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "2";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "3";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "4";
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "5";
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "6";
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "7";
+            
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "8";
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += "9";
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text += ".";
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            if (Resultado.Text != "")
+            {
+                valor1 = Decimal.Parse(Resultado.Text, CultureInfo.InvariantCulture);
+                Resultado.Text = "";
+                operacao1 = "MULTIPLICACAO";
+                operacao.Content = "x";
+            }        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            if (Resultado.Text != "")
+            {
+                valor1 = Decimal.Parse(Resultado.Text, CultureInfo.InvariantCulture);
+                Resultado.Text = "";
+                operacao1 = "DIVISAO";
+                operacao.Content = "/";
+            }
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            if (Resultado.Text != "")
+            {
+                valor1 = Decimal.Parse(Resultado.Text, CultureInfo.InvariantCulture);
+                Resultado.Text = "";
+                operacao1 = "SUBTRACAO";
+                operacao.Content = "-";
+            }
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            valor2 = Decimal.Parse(Resultado.Text, CultureInfo.InvariantCulture);
+
+            if(operacao1 == "SOMA")
+            {
+                Resultado.Text = Convert.ToString(valor1 + valor2);
+            }
+
+            if (operacao1 == "SUBTRACAO")
+            {
+                Resultado.Text = Convert.ToString(valor1 - valor2);
+            }
+            if (operacao1 == "DIVISAO")
+            {
+                Resultado.Text = Convert.ToString(valor1 / valor2);
+            }
+            if (operacao1 == "MULTIPLICACAO")
+            {
+                Resultado.Text = Convert.ToString(valor1 * valor2);
+            }
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text = "";
+            operacao.Content = "";
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+            Resultado.Text = "";
+            operacao.Content = "";
+            valor1 = 0;
+            valor2 = 0;
+            operacao1 = "";
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            if (Resultado.Text != "")
+            {
+                valor1 = Decimal.Parse(Resultado.Text, CultureInfo.InvariantCulture);
+                Resultado.Text = "";
+                operacao1 = "SOMA";
+                operacao.Content = "+";
+            }
+        }
+    }
+}
